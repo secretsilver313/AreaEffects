010e309ac63f91c9b5b3d0c5f31f1e565477a974
secretsilver313 <64828266+secretsilver313@users.noreply.github.com>
2020-05-23 10:09:47 -0400

    Update main.php

--- area/main.php
+++ area/main.php
@@ -1,1 +1,20 @@
+<?php
 
+namespace area;
+
+use pocketmine\entity\Effect;
+use pocketmine\entity\EffectInstance;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\level\Position;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\Player;
+use pocketmine\Server;
+
+public fuction onEnable() {
+}
+
+
+
+<
